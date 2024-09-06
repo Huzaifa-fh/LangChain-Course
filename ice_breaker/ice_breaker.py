8e26f8d632b4ab57540a8a4a71170d0c6575a8d4
huzaifafaheem <huzaifa.faheem@techlogix.com>
2024-09-06 12:35:38 +0500

    End to End implementation of LinkedIn Profile Summary Agent

--- ice_breaker/ice_breaker.py
+++ ice_breaker/ice_breaker.py
@@ -6,26 +6,32 @@ from dotenv import load_dotenv
 import os
 
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-if __name__ == "__main__":
-    load_dotenv()
-    print("Hello LangChain!")
+def ice_break_with(search_query: str) -> str:
+    linkedin_url = linkedin_lookup_agent(name=search_query)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_url)
 
     summary_template = """
-        given the LinkedIn information {information} about a person from I want you to create:
-        1. a short summary
-        2. two interesting facts about them
-    """
+            given the LinkedIn information {information} about a person from I want you to create:
+            1. a short summary
+            2. two interesting facts about them
+        """
 
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
     )
 
-    #llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
-    llm = ChatOllama(model="llama3")
-
+    # llm = ChatOllama(model="llama3")
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
     chain = summary_prompt_template | llm | StrOutputParser()
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url = "https://www.linkedin.com/in/huzaifa-faheem-17891623a/")
-    res = chain.invoke(input={"information": linkedin_data})
 
+    res = chain.invoke(input={"information": linkedin_data})
     print(res)
+    return res
+
+if __name__ == "__main__":
+    load_dotenv()
+    print("Ice Breaker Enter")
+
+    res = ice_break_with(search_query="Eden Marco Udemy")
---
+++ ice_breaker/tools/tools.py
@@ -0,0 +1,8 @@
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+def get_profile_url_tavily(search_query: str):
+    """Searches for Linkedin or Twitter Profile Page"""
+    search = TavilySearchResults()
+    res = search.run(f"{search_query}")
+    print(res)
+    return res[1]["url"]
